30d75b209e4f0b2c46747e686c44305a8dd30895
James Woodward Weis <jww@mit.edu>
2014-04-25 16:58:27 -0400

    Updated inference_engine.scm with simple_data test cases

--- inference_engine.scm
+++ inference_engine.scm
@@ -1,15 +1,30 @@
 ;;;; inference_engine.scm
 
-(define knowledge)
+(define all-knowledge)
 
-(define rules)
+(define all-rules)
 
 (define (ie:init)
-  (set! knowledge '())
-  (set! rules '()))
+  (set! all-knowledge '())
+  (set! all-rules '()))
+
 
 (define (ie:add-knowledge new-knowledge)
-  (append new-knowledge knowledge))
+  (set! all-knowledge
+        (append new-knowledge all-knowledge)))
 
 (define (ie:add-aliases new-aliases)
   (append new-aliases compound_obj_aliases))
+
+(define (ie:print-knowledge)
+  (pp all-knowledge))
+
+
+;; Tests
+(load "./simple_data/knowledge.scm")
+(pp knowledge)
+
+(ie:init)
+(ie:print-knowledge)
+(ie:add-knowledge knowledge)
+(ie:print-knowledge)
