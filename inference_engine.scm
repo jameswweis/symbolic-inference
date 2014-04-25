e9b3666dbd47c7d731e9d00296e1c999b361704f
James Woodward Weis <jww@mit.edu>
2014-04-25 17:41:54 -0400

    Now only non-new knowledge can be added to all-knowledge

--- inference_engine.scm
+++ inference_engine.scm
@@ -1,8 +1,6 @@
 ;;;; inference_engine.scm
 
-
-(load "pattern_matcher.scm")
-(define knowledge)
+(define all-knowledge)
 
 (define all-rules)
 
@@ -12,8 +10,9 @@
 
 
 (define (ie:add-knowledge new-knowledge)
-  (set! all-knowledge
-        (append new-knowledge all-knowledge)))
+  (let ((filtered-new-knowledge
+         (filter (lambda (x) (not (member x all-knowledge))) new-knowledge)))
+    (set! all-knowledge (append all-knowledge filtered-new-knowledge))))
 
 (define (ie:add-aliases new-aliases)
   (append new-aliases compound_obj_aliases))
@@ -25,11 +24,69 @@
   (pp all-knowledge))
 
 
+
 ;; Tests
 (load "./simple_data/knowledge.scm")
 (pp knowledge)
 
 (ie:init)
+;Value: ()
+
 (ie:print-knowledge)
-(ie:add-knowledge knowledge)
+; ()
+
+(define excess-knowledge (list
+(list 'CAUSE (list "shooting guards" 'score)
+      (list
+       (cons "title" "title1")
+       (cons "author" "author1")
+       (cons "year" "year1")
+       (cons "university" "univ1")
+       (cons "topic" "topic1")
+       (cons "journal" "journal1")
+       (cons "pubmed" "pubmed1")
+       (cons "locations" (list "loc_a1" "loc_b1"))))))
+;Value: excess-knowledge
+
+(ie:add-knowledge excess-knowledge)
+;Value: ()
+
 (ie:print-knowledge)
+;; ((cause
+;;   ("shooting guards" score)
+;;   (("title" . "title1")
+;;    ("author" . "author1")
+;;    ("year" . "year1")
+;;    ("university" . "univ1")
+;;    ("topic" . "topic1")
+;;    ("journal" . "journal1")
+;;    ("pubmed" . "pubmed1")
+;;    ("locations" "loc_a1" "loc_b1"))))
+
+(ie:add-knowledge excess-knowledge)
+;; ;Value: ((cause ("shooting guards" score)
+;; (("title" . "title1") ("author" . "author1") ("year" . "year1")
+;;  ("university" . "univ1") ("topic" . "topic1") ("journal" . "journal1")
+;;  ("pubmed" . "pubmed1") ("locations" "loc_a1" "loc_b1"))))
+
+(ie:print-knowledge)
+;; ((cause
+;;   ("shooting guards" score)
+;;   (("title" . "title1")
+;;    ("author" . "author1")
+;;    ("year" . "year1")
+;;    ("university" . "univ1")
+;;    ("topic" . "topic1")
+;;    ("journal" . "journal1")
+;;    ("pubmed" . "pubmed1")
+;;    ("locations" "loc_a1" "loc_b1"))))
+
+
+
+
+
+
+
+
+
+(ie:add-knowledge excess-knowledge)
