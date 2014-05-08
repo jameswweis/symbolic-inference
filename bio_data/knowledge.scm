480f4ff730a1189ae8a18797b03a3b107a4e1854
James Woodward Weis <jww@mit.edu>
2014-05-08 15:31:45 -0400

    Added hormone pathway to knowledge

--- bio_data/knowledge.scm
+++ bio_data/knowledge.scm
@@ -33,14 +33,25 @@
 
 (define wnt-pathway-context
   (list
-      (cons "title" "title1")
-      (cons "author" "author1")
-      (cons "year" "year1")
-      (cons "university" "univ1")
-      (cons "topic" "topic1")
-      (cons "journal" "journal1")
-      (cons "pubmed" "pubmed1")
-      (cons "locations" (list "loc_a1" "loc_b1"))))
+   (cons "title" "The Wnt signalling pathway")
+   (cons "author" "Huelsken, J., and Juergen, B.")
+   (cons "year" "2000")
+   (cons "university" "Max Delbrueck Center for Molecular Medicine")
+   (cons "topic" "Cell biology")
+   (cons "journal" "J. Cell Sci.")
+   (cons "pubmed" "Unknown.")
+   (cons "locations" (list "loc_a1" "loc_b1"))))
+
+(define hormone-context
+  (list
+   (cons "title" "The Hallmarks of Cancer")
+   (cons "author" "Hanahan, D., and Weinberg, R.A.")
+   (cons "year" "2000")
+   (cons "university" "University of California at San Francisco")
+   (cons "topic" "Cancer")
+   (cons "journal" "Cell")
+   (cons "pubmed" "10647931")
+   (cons "locations" (list "loc_a1" "loc_b1"))))
 
 (define knowledge
   (list
@@ -52,7 +63,15 @@
    '(CAUSE (APC Beta-Cetenin) wnt-pathway-context)
    '(CAUSE (Other-cell E-Cadherin Beta-Catenin) wnt-pathway-context)
    '(CAUSE (Beta-Catenin TCF Beta-Catenin:TCF) wnt-pathway-context)
-   '(CAUSE (Beta-Catenin:TCF 'Changes-in-gene-expression))
+   '(CAUSE (Beta-Catenin:TCF Changes-in-gene-expression))
 
+   ;; Hormone pathway
+   '(CAUSE (Estrogen ER) hormone-context)
+   '(CAUSE (ER Changes-in-gene-expression) hormone-context)
+   '(CAUSE (Bombesin 7-TMR G-protein) hormone-context)
+   '(CAUSE (G-protein Ad-Cyclin) hormone-context)
+   '(CAUSE (Ad-Cyclin PKA) hormone-context)
+   '(CAUSE (PKA CREB) hormone-context)
+   '(CAUSE (CREB Changes-in-gene-expression) hormone-context)
 
    ))
