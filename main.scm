ba3b768b727dd150dee91d59f81f143cc7d8abb7
leoliu1992 <leoliu_cu@sbcglobal.net>
2014-05-09 22:18:48 +0000

    refactoring

--- main.scm
+++ main.scm
@@ -1,15 +1,16 @@
-;(ie:add-rules new-rules);;;; main.scm
+;;;; main.scm
 
 (load "inference_engine.scm")
 
-(load "data/cancer_biology/knowledge.scm")
-(load "data/cancer_biology/rules.scm")
-
 (ie:init)
 
-(ie:add-knowledge2 knowledge)
+(load "data/simple/knowledge.scm")
+(load "data/simple/rules.scm")
+
+(ie:add-knowledge knowledge)
+(ie:add-aliases compound_obj_aliases)
 (ie:add-rules rules)
 
-;(ie:is-true2 (list 'CAUSE (list "shooting guards" 'win)) '())
-(ie:infer '())
-(ie:print-knowledge)
+(ie:is-true (list 'CAUSE (list "shooting guards" 'win)) '())
+;(ie:infer '())
+;(ie:print-knowledge)
