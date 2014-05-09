8da476abde92cb294ce30de943a798ea171d26a0
James Woodward Weis <jww@mit.edu>
2014-05-09 15:54:28 -0400

    Added more bio knowledge

--- bio_data/knowledge.scm
+++ bio_data/knowledge.scm
@@ -1,35 +1,6 @@
 ;; Biological data
 
-;; (define compound_obj_aliases
-;;   (list
-;;    (cons "shooting guards" (list 'kobe 'wade 'mj 'jordan))
-;;    ))
-
-;; (define simple-context
-;;   (list
-;;    (cons "title" "title1")
-;;    (cons "author" "author1")
-;;    (cons "year" "year1")
-;;    (cons "university" "univ1")
-;;    (cons "topic" "topic1")
-;;    (cons "journal" "journal1")
-;;    (cons "pubmed" "pubmed1")
-;;    (cons "locations" (list "loc_a1" "loc_b1"))
-;;    ))
-
-;; (define knowledge
-;;   (list
-;;    (list 'CAUSE '(thing-1 thing-2) simple-context)
-;;    (list 'CAUSE '(thing-2 thing-3) simple-context)
-;;    (list 'CAUSE '(thing-2 thing-4) simple-context)
-;;    (list 'UPREGULATE '(monster-1 monster-2) simple-context)
-;;    (list 'DOWNREGULATE '(monster-2 monster-3) simple-context)
-;;    (list 'DOWNREGULATE '(monster-2 monster-4) simple-context)
-;;    ))
-
-;; (pp compound_obj_aliases)
-;; (pp knowledge)
-;; (pp "knowledge done")
+(define compound_obj_aliases (list))
 
 (define wnt-pathway-context
   (list
@@ -126,5 +97,9 @@
    '(BLOCK (p21 Cyclin-E:CDK2) tgfb-context)
 
 
-
    ))
+
+
+(pp compound_obj_aliases)
+(pp knowledge)
+(pp "knowledge done")
